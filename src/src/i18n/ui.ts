2b79621cc471c5347d069c960cc717aef9e717e6
tv-23 <tvelqjiri@gmail.com>
2026-01-22 14:08:15 +0100

    gfjgk

--- src/src/i18n/ui.ts
+++ src/src/i18n/ui.ts
@@ -17,22 +17,22 @@ export const ui = {
     'nav.call': 'Call us',
 
     // Slider
-    'sliders.items':[
+    'sliders.items': [
       {
-        title_1:'Sea Freight And Shipping',
-        title_2:'Smart Ocean Freight Solutions Worldwide',
+        title_1: 'Sea Freight And Shipping',
+        title_2: 'Smart Ocean Freight Solutions Worldwide',
       },
       {
-        title_1:'Road And Trucking Logistics',
-        title_2:'Reliable Trucking for Every Mile',
+        title_1: 'Road And Trucking Logistics',
+        title_2: 'Reliable Trucking for Every Mile',
       },
       {
-        title_1:'Shipping Agency Services',
-        title_2:'Professional Port And Vessel Agency Solutions',
+        title_1: 'Shipping Agency Services',
+        title_2: 'Professional Port And Vessel Agency Solutions',
       },
       {
-        title_1:'Air Freight Solutions',
-        title_2:'Smart Ocean Freight Solutions Worldwide',
+        title_1: 'Air Freight Solutions',
+        title_2: 'Smart Ocean Freight Solutions Worldwide',
       },
     ],
 
@@ -46,91 +46,91 @@ export const ui = {
     'about.content.values': 'We operate with strict compliance and integrity, ensuring ethics, transparency, and responsibility at every stage. We pursue operational excellence through high standards of execution, accuracy, service quality, and respect for deadlines, while prioritizing client and partner satisfaction through attentive listening, responsiveness, and tailored support.',
 
     // Our Values
-    'our_values.secion.name':{
-      'p1':'Our',
-      'p2':'Values',
+    'our_values.secion.name': {
+      'p1': 'Our',
+      'p2': 'Values',
     },
     'our_values.secion.items': [
       {
-        nbr:"01",
+        nbr: "01",
         title: 'Compliance & Integrity',
         description: 'We operate with full respect for regulations, ethics, transparency, and accountability.'
       },
-       {
-        nbr:"02",
+      {
+        nbr: "02",
         title: 'Operational Excellence',
         description: 'We uphold the highest standards in execution quality, precision, reliability, and on-time performance.'
       },
-     {
-        nbr:"03",
+      {
+        nbr: "03",
         title: 'Customer Commitment & Partnership',
         description: 'Client satisfaction is central to our approach through responsiveness, tailored support, and a service culture built on trust.'
       },
       {
-        nbr:"04",
+        nbr: "04",
         title: 'Proximity & Team Spirit',
         description: 'We believe in strong partnerships, continuous communication, and on-the-ground follow-up as one integrated team.'
       },
-       {
-        nbr:"05",
+      {
+        nbr: "05",
         title: 'Performance & Continuous Improvement',
         description: 'We continuously optimize processes, anticipate risks, and drive efficiency to deliver measurable results.'
       },
       {
-        nbr:"06",
+        nbr: "06",
         title: ' Innovation & Digitalization',
         description: 'We embrace modern tools and digital approaches to strengthen tracking, visibility, reporting, and operational transparency.'
       },
     ],
 
     // Our Companies
-    'our_companies.secion.name':{
-      'p1':'Our',
-      'p2':'Companies',
+    'our_companies.secion.name': {
+      'p1': 'Our',
+      'p2': 'Companies',
     },
-    'our_companies.secion.btn':'Discover',
+    'our_companies.secion.btn': 'Discover',
     'our_companies.secion.items': [
       {
-        img_1:"images/companies/unireef.jpg",
-        img_2:"/images/companies/3.jpg",
+        img_1: "images/companies/unireef.jpg",
+        img_2: "/images/companies/3.jpg",
         title: 'Unireef',
         description: 'Unireef is a shipping company specializing in ship chartering and global transport solutions. With strong expertise in reefer shipping, it ensures fast and dedicated export of Moroccan citrus and vegetables.',
-        url:'https://unireef.ma/'
+        url: 'https://unireef.ma/'
       },
       {
-        img_1:"images/companies/Mss.jpg",
-        img_2:"/images/companies/1.jpg",
+        img_1: "images/companies/Mss.jpg",
+        img_2: "/images/companies/1.jpg",
         title: 'MSS',
         description: 'Maritime Ship Services provides ship agency, brokerage, and freight forwarding services across major Moroccan ports. With strong operational expertise, the company ensures efficient vessel handling and optimized transport solutions.',
-        url:'https://mss.ma/'
+        url: 'https://mss.ma/'
       },
       {
-        img_1:"images/companies/Internavi.jpg",
-        img_2:"/images/companies/5.jpg",
+        img_1: "images/companies/Internavi.jpg",
+        img_2: "/images/companies/5.jpg",
         title: 'INTERNAVI',
         description: "As a port handling operator in Agadir and shareholder of the second terminal, INTERNAVI provides efficient cargo handling and port storage services through proven expertise and modern cargo management.",
-        url:'https://internavi.ma/'
+        url: 'https://internavi.ma/'
       },
       {
-        img_1:"images/companies/own.jpg",
-        img_2:"/images/companies/2.jpg",
+        img_1: "images/companies/own.jpg",
+        img_2: "/images/companies/2.jpg",
         title: 'OWN',
         description: "OWN provides customs clearance, freight forwarding, warehousing, and transport services for import and export operations. Its digitalized processes ensure fast and efficient cargo handling at ports and airports.",
-        url:'https://own.ma/'
+        url: 'https://own.ma/'
       },
       {
-        img_1:"images/companies/bst.png",
-        img_2:"/images/companies/4.jpg",
+        img_1: "images/companies/bst.png",
+        img_2: "/images/companies/4.jpg",
         title: 'BTS',
         description: "BTS Control is an independent inspection and survey company providing verification and control services for goods and vessels. Its expertise and compliance with international standards ensure reliable and accurate inspections.",
-        url:'https://bts.ma/'
+        url: 'https://bts.ma/'
       },
     ],
 
     // Why SEATRUST
-    'why_seatrust.secion.name':{
-      'p1':'Why',
-      'p2':'SEATRUST ?',
+    'why_seatrust.secion.name': {
+      'p1': 'Why',
+      'p2': 'SEATRUST ?',
     },
     'why_seatrust.secion.items': [
       {
@@ -163,8 +163,54 @@ export const ui = {
         title: 'Tailor-Made Solutions for Every Sector',
         description: 'We design adapted solutions for each sector and shipment type (Agricultural, Agri-food, industrial, commercial,...), ensuring flexibility, service excellence, and client satisfaction.'
       },
+    ],
+
+    // Where we are
+    'where_we_are.secion.name': {
+      'p1': 'Where',
+      'p2': 'we are ?',
+    },
+    'where_we_are.secion.btns': [
+      {
+        title: 'Main offices (7)'
+      },
+      {
+        title: 'Global Connections (24)'
+      },
+      {
+        title: 'Track Ship Path'
+      },
+    ],
+    'where_we_are.secion.form': [
+      {
+        labels: [
+          {
+            title: 'ORIGIN (MOROCCO)'
+          },
+          {
+            title: 'DESTINATION (WORLD)'
+          }, {
+            title: 'EMAIL ADDRESS'
+          }
+          , {
+            title: 'PHONE NUMBER'
+          }
+        ]
+      },
+      {
+        button: 'Calculate Route'
+      }
+    ],
+
+    // CTA
+    'cta': [
+      {
+        heading: 'Ready to optimize your logistics operations?',
+        paragraph: 'End-to-end logistics solutions, caring about quality, reliability, and operational excellence.',
+        button: 'Contact our experts',
+      },
     ]
-    
+
   },
   fr: {
     'header.tagline': 'SEATRUST LOGISTICS — Transports & Logistiques Maritimes',
@@ -174,7 +220,7 @@ export const ui = {
     'nav.contact': 'Contactez-nous',
     'nav.call': 'Nous appeler',
 
- 
+
     // Slider
     'sliders.items': [
       {
@@ -194,7 +240,7 @@ export const ui = {
         title_2: 'Solutions intelligentes de fret aérien à l’échelle mondiale',
       },
     ],
-    
+
 
     'about.tab.seatrust': 'Seatrust',
     'about.tab.mission': 'Notre Mission',
@@ -205,122 +251,167 @@ export const ui = {
 
 
     // Our Values
-    'our_values.secion.name':{
-      'p1':'Nos',
-      'p2':'Valeurs',
+    'our_values.secion.name': {
+      'p1': 'Nos',
+      'p2': 'Valeurs',
     },
     'our_values.secion.items': [
       {
-        nbr:"01",
+        nbr: "01",
         title: 'Conformité & intégrité',
         description: 'Nous respectons strictement les réglementations et opérons avec éthique, transparence et responsabilité.'
       },
-       {
-        nbr:"02",
+      {
+        nbr: "02",
         title: 'Excellence opérationnelle',
         description: 'Nous visons un haut niveau d’exigence dans l’exécution, la précision, la qualité de service et la gestion des délais.'
       },
-     {
-        nbr:"03",
+      {
+        nbr: "03",
         title: 'Engagement client & partenariat',
         description: 'La satisfaction de nos partenaires est une priorité : écoute, réactivité, accompagnement et solutions sur mesure.'
       },
       {
-        nbr:"04",
+        nbr: "04",
         title: 'Proximité & esprit d’équipe',
         description: 'Nous privilégions une collaboration fluide et humaine, fondée sur la confiance, le suivi terrain et la communication continue.'
       },
-       {
-        nbr:"05",
+      {
+        nbr: "05",
         title: 'Performance & amélioration continue',
         description: 'Nous optimisons nos processus, anticipons les risques et recherchons constamment l’efficacité et la performance.'
       },
       {
-        nbr:"06",
+        nbr: "06",
         title: 'Innovation & digitalisation',
         description: 'Nous intégrons des outils et méthodes modernes pour renforcer la traçabilité, le pilotage et la transparence des opérations.'
       },
     ],
 
     // Our Companies
-    'our_companies.secion.name':{
-      'p1':'Nos',
-      'p2':'Entreprises',
+    'our_companies.secion.name': {
+      'p1': 'Nos',
+      'p2': 'Entreprises',
     },
-    'our_companies.secion.btn':'Découvrir',
+    'our_companies.secion.btn': 'Découvrir',
     'our_companies.secion.items': [
       {
-        img_1:"images/companies/unireef.jpg",
-        img_2:"/images/companies/3.jpg",
+        img_1: "images/companies/unireef.jpg",
+        img_2: "/images/companies/3.jpg",
         title: 'Unireef',
         description: 'Unireef est une compagnie maritime spécialisée dans l’affrètement de navires et les solutions de transport international. Elle assure l’exportation rapide et dédiée des agrumes et légumes marocains par navires frigorifiques.',
-        url:'https://unireef.ma/'
+        url: 'https://unireef.ma/'
       },
       {
-        img_1:"images/companies/Mss.jpg",
-        img_2:"/images/companies/1.jpg",
+        img_1: "images/companies/Mss.jpg",
+        img_2: "/images/companies/1.jpg",
         title: 'MSS',
         description: 'Maritime Ship Services propose des services d’agence maritime, de courtage et de freight forwarding dans les principaux ports marocains. L’entreprise garantit une gestion efficace des navires et des solutions de transport optimisées.',
-        url:'https://mss.ma/'
+        url: 'https://mss.ma/'
       },
       {
-        img_1:"images/companies/Internavi.jpg",
-        img_2:"/images/companies/5.jpg",
+        img_1: "images/companies/Internavi.jpg",
+        img_2: "/images/companies/5.jpg",
         title: 'INTERNAVI',
         description: "Opérateur de manutention au port d’Agadir et actionnaire du second terminal, INTERNAVI dispose du savoir-faire nécessaire pour assurer efficacement les opérations de manutention et d’entreposage portuaire.",
-        url:'https://internavi.ma/'
+        url: 'https://internavi.ma/'
       },
       {
-        img_1:"images/companies/own.jpg",
-        img_2:"/images/companies/2.jpg",
+        img_1: "images/companies/own.jpg",
+        img_2: "/images/companies/2.jpg",
         title: 'OWN',
         description: "OWN propose des services de dédouanement, de freight forwarding, d’entreposage et de transport pour les opérations d’import-export. La digitalisation de ses processus garantit un traitement rapide et efficace des marchandises dans les ports et aéroports.",
-        url:'https://own.ma/'
+        url: 'https://own.ma/'
       },
       {
-        img_1:"images/companies/bst.png",
-        img_2:"/images/companies/4.jpg",
+        img_1: "images/companies/bst.png",
+        img_2: "/images/companies/4.jpg",
         title: 'BTS',
         description: "BTS Control est une société indépendante d’inspection et de contrôle, spécialisée dans la vérification des marchandises et des navires. Son expertise et le respect des normes internationales garantissent des inspections fiables et précises.",
-        url:'https://bts.ma/'
+        url: 'https://bts.ma/'
       },
     ],
 
     // Why SEATRUST
-    'why_seatrust.secion.name':{
-      'p1':'Pourquoi',
-      'p2':'SEATRUST ?',
+    'why_seatrust.secion.name': {
+      'p1': 'Pourquoi',
+      'p2': 'SEATRUST ?',
     },
     'why_seatrust.secion.items': [
       {
-        nbr:'01',
-        title:'Pilotage logistique End-to-End (de bout en bout)',
-        description:'Une prise en charge complète des opérations import/export : transport, transit, dédouanement et coordination terrain, avec un pilotage structuré et maîtrisé.'
+        nbr: '01',
+        title: 'Pilotage logistique End-to-End (de bout en bout)',
+        description: 'Une prise en charge complète des opérations import/export : transport, transit, dédouanement et coordination terrain, avec un pilotage structuré et maîtrisé.'
       },
       {
-        nbr:'02',
-        title:'Présence directe sur l’ensemble des ports marocains',
-        description:'Une présence directe sur l’ensemble des ports marocains, garantissant réactivité maximale, intervention immédiate et continuité opérationnelle.'
+        nbr: '02',
+        title: 'Présence directe sur l’ensemble des ports marocains',
+        description: 'Une présence directe sur l’ensemble des ports marocains, garantissant réactivité maximale, intervention immédiate et continuité opérationnelle.'
       },
       {
-        nbr:'03',
-        title:'Expertise douanière & maîtrise de la conformité',
-        description:'Une expertise approfondie des procédures douanières et réglementaires, permettant sécurisation des dossiers, réduction des risques et conformité totale.'
+        nbr: '03',
+        title: 'Expertise douanière & maîtrise de la conformité',
+        description: 'Une expertise approfondie des procédures douanières et réglementaires, permettant sécurisation des dossiers, réduction des risques et conformité totale.'
       },
       {
-        nbr:'04',
-        title:'Excellence opérationnelle & respect des délais',
-        description:'Une organisation orientée performance : délais maîtrisés, exécution rigoureuse, réduction des temps d’attente et optimisation des coûts indirects.'
+        nbr: '04',
+        title: 'Excellence opérationnelle & respect des délais',
+        description: 'Une organisation orientée performance : délais maîtrisés, exécution rigoureuse, réduction des temps d’attente et optimisation des coûts indirects.'
       },
       {
-        nbr:'05',
-        title:'Suivi en temps réel & communication transparente',
-        description:'Un suivi opérationnel clair et fluide, avec visibilité, reporting et communication proactive, pour une prise de décision rapide.'
+        nbr: '05',
+        title: 'Suivi en temps réel & communication transparente',
+        description: 'Un suivi opérationnel clair et fluide, avec visibilité, reporting et communication proactive, pour une prise de décision rapide.'
       },
       {
-        nbr:'06',
-        title:'Solutions logistique sur mesure adaptées à chaque secteur',
-        description:'Des solutions adaptées aux besoins et contraintes de chaque secteur (Agro, industrie, distribution…), avec une approche flexible orientée résultats.'
+        nbr: '06',
+        title: 'Solutions logistique sur mesure adaptées à chaque secteur',
+        description: 'Des solutions adaptées aux besoins et contraintes de chaque secteur (Agro, industrie, distribution…), avec une approche flexible orientée résultats.'
+      },
+    ],
+    // Where we are
+    'where_we_are.secion.name': {
+      p1: 'Où',
+      p2: 'sommes-nous ?',
+    },
+    'where_we_are.secion.btns': [
+      {
+        title: 'Bureaux principaux (7)'
+      },
+      {
+        title: 'Connexions internationales (24)'
+      },
+      {
+        title: 'Suivre le trajet du navire'
+      },
+    ],
+    'where_we_are.secion.form': [
+      {
+        labels: [
+          {
+            title: 'ORIGINE (MAROC)'
+          },
+          {
+            title: 'DESTINATION (MONDE)'
+          },
+          {
+            title: 'ADRESSE E-MAIL'
+          },
+          {
+            title: 'NUMÉRO DE TÉLÉPHONE'
+          }
+        ]
+      },
+      {
+        button: 'Calculer l’itinéraire'
+      }
+    ],
+    // CTA
+    'cta': [
+      {
+        heading: 'Prêt à optimiser vos opérations logistiques ?',
+        paragraph: 'Des solutions logistiques complètes, avec une exigence forte de qualité et de fiabilité.',
+        button: 'Contacter nos experts',
       },
     ]
   },
